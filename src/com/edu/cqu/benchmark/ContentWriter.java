185f02ef1d098f1d11dbea04be38988cc5912c56
Lizhen Liu <llizen.joy@gmail.com>
2018-08-27 14:25:16 +0800

    问题变量定义

--- src/com/edu/cqu/benchmark/ContentWriter.java
+++ src/com/edu/cqu/benchmark/ContentWriter.java
@@ -1,4 +1,11 @@
 package com.edu.cqu.benchmark;
 
 public class ContentWriter {
+    public static final String PROBLEM_SCALE_FREE_NETWORK = "SCALE_FREE_NETWORK";
+    public static final String PROBLEM_RANDOM_DCOP = "RANDOM_DCOP";
+    public static final String PROBLEM_GRAPH_COLORING = "GRAPH_COLORING";
+    public static final String PROBLEM_WEIGHTED_GRAPH_COLORING = "WEIGHTED_GRAPH_COLORING";
+    public static final String PROBLEM_RANDOM_ADCOP = "RANDOM_ADCOP";
+    public static final String PROBLEM_ASYMETIRC_MAX_DCSP = "RANDOM_A_MAX_DCSP";
+    public static final String PROBLEM_MEETING_SCHEDULING = "MEETING_SCHEDULING";
 }
